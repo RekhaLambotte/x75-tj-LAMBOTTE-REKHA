60065cd4096bc47459a3d1bf0b251a0f36c1c04e
RekhaLambotte <rekhalambotte246@gmail.com>
2022-01-24 21:46:33 +0100

    1.Crer un composant MyIdentity avec deux proprits (firstname,lastname)

--- MyfirstApp/src/app/app.module.ts
+++ MyfirstApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyIdentityComponent } from './my-identity/my-identity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyIdentityComponent
   ],
   imports: [
     BrowserModule,
