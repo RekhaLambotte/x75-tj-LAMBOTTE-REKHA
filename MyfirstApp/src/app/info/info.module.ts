b401b3289b2b7bdac63769791061ac895119c5d4
RekhaLambotte <rekhalambotte246@gmail.com>
2022-01-25 22:55:08 +0100

    2.2.+ 2.3 Créer un système de routing dans l’infoModule + Associer MyIdentity, MyDescription, InfoPage dans le module InfoModule

--- MyfirstApp/src/app/info/info.module.ts
+++ MyfirstApp/src/app/info/info.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {InfoPageComponent} from "./component/info-page/info-page.component";
 import {MyIdentityComponent} from "./component/my-identity/my-identity.component";
 import {MyDescriptionComponent} from "./component/my-description/my-description.component";
+import {InfoRoutingModule} from "./info-routing.module";
 
 @NgModule({
-  declarations: [InfoPageComponent, MyIdentityComponent, MyDescriptionComponent],
+  declarations: [ InfoPageComponent, MyIdentityComponent, MyDescriptionComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    InfoRoutingModule
   ]
 })
 export class InfoModule { }
