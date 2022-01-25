ec0de7140c12470bd8ab9ccda638a165fa9af071
RekhaLambotte <rekhalambotte246@gmail.com>
2022-01-25 17:48:01 +0100

    5.Crer un module InfoModule + regroupement components

--- MyfirstApp/src/app/app.module.ts
+++ MyfirstApp/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyIdentityComponent } from './my-identity/my-identity.component';
-import { MyDescriptionComponent } from './my-description/my-description.component';
-import { InfoPageComponent } from './info-page/info-page.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyIdentityComponent } from './component/my-identity/my-identity.component';
+import { MyDescriptionComponent } from './component/my-description/my-description.component';
+import { InfoPageComponent } from './component/info-page/info-page.component';
+import { ContactPageComponent } from './component/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
